b285689dd02f6d4bfb84282c3225db07146417e6
deepxshine <aevdok2800@gmail.com>
2024-02-18 12:06:38 +0300

    v2

--- kokoc_group/rates/views.py
+++ kokoc_group/rates/views.py
@@ -7,10 +7,16 @@ from .models import Rate
 def index(request):
     date = request.GET.get('date')
     charcode = request.GET.get('charcode')
-    rate = get_object_or_404(Rate, date=date, charcode=charcode)
-    data = {
-        'date': date,
-        'charcode': charcode,
-        'rate': rate.rate
-    }
-    return JsonResponse(data=data, json_dumps_params={'ensure_ascii': False})
+    rate = Rate.objects.filter(date=date, charcode=charcode).first()
+    if rate:
+        data = {
+            'date': date,
+            'charcode': charcode,
+            'rate': rate.rate
+        }
+        return JsonResponse(data=data, json_dumps_params={'ensure_ascii': 
+                                                          False})
+    return JsonResponse(data={
+        'status': '404 Not Found',
+        'detail': ('Данные по вашему запросу не найлены')
+    }, json_dumps_params={'ensure_ascii': False})
